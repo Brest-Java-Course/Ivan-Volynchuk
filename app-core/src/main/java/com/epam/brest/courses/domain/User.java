d207e28145f015e388e7fda564cd4e54edfbf454
Fieldistor <codesmint@gmail.com>
2014-10-17 20:21:46 +0400

    get_and_set_here

--- app-core/src/main/java/com/epam/brest/courses/domain/User.java
+++ app-core/src/main/java/com/epam/brest/courses/domain/User.java
@@ -3,16 +3,33 @@ package com.epam.brest.courses.domain;
 //smth new from Idea
 public class User{
 
-	private Long userId;
-	private String login;
-	private String userName;
-	
-	public Long getUserId(){
-	return userId;
-	}
 
-	public void setUserId(Long userId){
-	this.userId=userId;		
-	}
-	
+    private Long userId;
+    private String login;
+    private String userName;
+
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
