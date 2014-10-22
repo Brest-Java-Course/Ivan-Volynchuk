46230bd13637b2b111f4248bc0a4d5f64b0cd08c
Fieldistor <codesmint@gmail.com>
2014-10-22 21:33:58 +0400

    Tests are ok

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDaoImpl.java
@@ -1,10 +1,15 @@
 package com.epam.brest.courses.dao;
 
 import com.epam.brest.courses.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 import javax.sql.DataSource;
+
+import javax.swing.tree.TreePath;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
+import org.springframework.jdbc.core.*;
 
 /**
  * Created by fieldistor on 20.10.14.
@@ -24,12 +29,22 @@ public class UserDaoImpl implements UserDao{
 
     @Override
     public List<User> getUsers() {
-     return null;
-     // return jdbcTemplate.query("SELECT userid, login, name from USER",null);
+    return jdbcTemplate.query("SELECT userid, login, name from USER",new UserMapper());
     }
 
     @Override
     public void removeUser(Long userId) {
 
+    }
+
+    public class UserMapper implements RowMapper<User>{
+        @Override
+        public User mapRow(ResultSet resultSet,int i) throws SQLException{
+            User user=new User();
+            user.setUserId(resultSet.getLong("userId"));
+            user.setLogin(resultSet.getString("login"));
+            user.setUserName(resultSet.getString("name"));
+            return user;
+        }
     }
 }
