302875ae745523877885400d5dad46c01b3eb2f4
Fieldistor <codesmint@gmail.com>
2014-10-27 22:40:15 +0400

    Parameterized JUnit, EasyMock added.

--- app-service/src/test/java/com/epam/brest/courses/service/UserServiceImplMockTest.java
+++ app-service/src/test/java/com/epam/brest/courses/service/UserServiceImplMockTest.java
@@ -2,6 +2,10 @@ package com.epam.brest.courses.service;
 
 import com.epam.brest.courses.dao.UserDao;
 import com.epam.brest.courses.domain.User;
+import junit.framework.Assert;
+import org.junit.After;
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +25,10 @@ public class UserServiceImplMockTest {
     @Autowired
     private UserDao userDao;
 
+    @After
+    public void clean(){
+        reset(userDao);
+    }
     @Test
     public void addUser() {
         User user= UserDataFixture.getNewUser();
@@ -32,7 +40,69 @@ public class UserServiceImplMockTest {
 
         replay(userDao);
         userService.addUser(user);
-
         verify(userDao);
     }
+
+    @Test
+    public void addUser2() {
+        User user= UserDataFixture.getNewUser();
+
+        userDao.getUserByLogin(user.getLogin());
+        expectLastCall().andReturn(null);
+        userDao.addUser(user);
+        expectLastCall();
+
+        userDao.getUserByLogin(user.getLogin());
+        expectLastCall().andReturn(null);
+        userDao.addUser(user);
+        expectLastCall();
+
+
+
+        replay(userDao);
+        userService.addUser(user);
+        userService.addUser(user);
+        verify(userDao);
+    }
+
+    @Test
+    public void UserByLogin() {
+        User user=UserDataFixture.getExistUser(55L);
+
+        expect(userDao.getUserByLogin(user.getLogin())).andReturn(user);
+
+
+        replay(userDao);
+        User result=userService.getUserByLogin(user.getLogin());
+        verify(userDao);
+
+        assertSame(user, result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void addUserWithTheSameLogin() {
+        User user=UserDataFixture.getNewUser();
+
+
+        expect(userDao.getUserByLogin(user.getLogin())).andReturn(user);
+
+
+
+
+        replay(userDao);
+        userService.addUser(user);
+
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void addUserException() {
+        User user=UserDataFixture.getNewUser();
+
+        expect(userDao.getUserByLogin(user.getLogin())).andThrow(new NumberFormatException());
+
+        replay(userDao);
+        userService.addUser(user);
+    }
+
+
 }
