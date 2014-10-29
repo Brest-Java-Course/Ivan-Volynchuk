fdd853186f0356bd33a9b650dd3b68f69e44b51a
Fieldistor <codesmint@gmail.com>
2014-10-29 22:34:43 +0400

    sql files plus XML context refactoring.

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDao.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDao.java
@@ -5,10 +5,19 @@ import java.util.List;
  * Created by fieldistor on 20.10.14.
  */
 public interface UserDao {
+    /**
+     * Create new user.
+     * @param user data of user
+     */
     public void addUser(User user);
+
     public List<User> getUsers();
+
     public void removeUser (Long userId);
+
     public User getUserById(Long userId);
+
     public User getUserByLogin(String login);
+
     public void updateUser(User user);
 }
