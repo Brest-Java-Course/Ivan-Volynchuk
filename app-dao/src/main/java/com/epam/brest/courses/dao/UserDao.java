89767691243352a2ac48e6a0dc0d0a978cdd15fe
Fieldistor <codesmint@gmail.com>
2014-10-30 02:03:27 +0400

    Comments (interfaces), sql (.sql files and xml configuration), some test(Service Junit Tests) added.

--- app-dao/src/main/java/com/epam/brest/courses/dao/UserDao.java
+++ app-dao/src/main/java/com/epam/brest/courses/dao/UserDao.java
@@ -5,19 +5,52 @@ import java.util.List;
  * Created by fieldistor on 20.10.14.
  */
 public interface UserDao {
+
     /**
-     * Create new user.
-     * @param user data of user
+     * The method used for adding the user to base.
+     *
+     * @param user the data(id, login, name) to be added
      */
     public void addUser(User user);
 
+    /**
+     * The method returns a list of users that can then be processed.
+     * This method always returns immediately, whether or not any users exist.
+     *
+     * @return a list of users
+     */
     public List<User> getUsers();
 
+    /**
+     * The method used for deleting the user from base by id.
+     *
+     * @param userId the id of deleting user
+     */
     public void removeUser (Long userId);
 
+    /**
+     * The method used for getting the user from base by id.
+     *
+     * @exception org.springframework.dao.EmptyResultDataAccessException If an id is null or 0
+     * @param userId id of getting user
+     * @return data of getting user
+     */
     public User getUserById(Long userId);
 
+    /**
+     * The method used for getting the user from base by login.
+     * This method always returns immediately, whether or not such user exists.
+     *
+     * @exception org.springframework.dao.EmptyResultDataAccessException If login is null or incorrect
+     * @param login login of getting user
+     * @return data of getting user
+     */
     public User getUserByLogin(String login);
 
+    /**
+     * The method used for updating user.
+     *
+     * @param user id, login ,name to update the specific user
+     */
     public void updateUser(User user);
 }
